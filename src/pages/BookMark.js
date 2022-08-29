9abb2b7837c711f33043543c777afbbc070adbcc
이지영 <jssq2468@naver.com>
2022-08-29 16:25:49 +0900

    feat : 즐겨찾기 스타일 추가

--- src/pages/BookMark.js
+++ src/pages/BookMark.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeStar } from "../redux/favorite.js";
-
+import { Container } from "../Style/Box";
+import imgA from "../images/좋음.png";
+import imgB from "../images/보통.png";
+import imgC from "../images/나쁨.png";
 function BookMark() {
   let [countstar, setcountstar] = useState([false]);
   console.log(countstar);
@@ -10,24 +13,77 @@ function BookMark() {
     return state.favorite;
   });
   let dispatch = useDispatch();
-
+  const deleteStar = (i) => {
+    let copy = [...countstar];
+    copy[i] = !copy[i];
+    setcountstar(copy);
+    dispatch(removeStar(i));
+  };
   const item = state.map((a, i) => {
     if (a.star) {
       return (
-        <div key={Math.floor(Math.random() * 100000)}>
-          <p>{a.stationName}</p>
-          <button
-            onClick={() => {
-              let copy = [...countstar];
-              copy[i] = !copy[i];
-              setcountstar(copy);
-              dispatch(removeStar(i));
-            }}
-          >
-            {a.star ? <p>★</p> : <p>☆</p>}
-          </button>
-          <p>{a.star}</p>
-        </div>
+        <Container>
+          <div key={Math.floor(Math.random() * 100000)}>
+            {a.pm10Grade == "1" ? (
+              <div className="bgcolor1">
+                <div className="header">
+                  <button
+                    onClick={() => {
+                      deleteStar(i);
+                    }}
+                  >
+                    {a.star ? <p>★</p> : <p>☆</p>}
+                  </button>
+
+                  <p>{a.star}</p>
+                </div>
+                <p>{a.stationName}</p>
+                <p>{a.sidoName}</p>
+                <p>{a.time}</p>
+                <img src={imgA} style={{ width: "20px" }} />
+                <p>좋음</p>
+              </div>
+            ) : a.pm10Grade === "2" || a.pm10Grade === "3" ? (
+              <div className="bgcolor2">
+                <div className="header">
+                  <button
+                    onClick={() => {
+                      deleteStar(i);
+                    }}
+                  >
+                    {a.star ? <p>★</p> : <p>☆</p>}
+                  </button>
+
+                  <p>{a.star}</p>
+                </div>
+                <p>{a.stationName}</p>
+                <p>{a.sidoName}</p>
+                <p>{a.time}</p>
+                <img src={imgB} style={{ width: "20px" }} />
+                <p>보통</p>
+              </div>
+            ) : (
+              <div className="bgcolor3">
+                <div className="header">
+                  <button
+                    onClick={() => {
+                      deleteStar(i);
+                    }}
+                  >
+                    {a.star ? <p>★</p> : <p>☆</p>}
+                  </button>
+
+                  <p>{a.star}</p>
+                </div>
+                <p>{a.stationName}</p>
+                <p>{a.sidoName}</p>
+                <p>{a.time}</p>
+                <img src={imgC} style={{ width: "20px" }} />
+                <p>나쁨</p>
+              </div>
+            )}
+          </div>
+        </Container>
       );
     } else if (a.star == false) {
       return null;
